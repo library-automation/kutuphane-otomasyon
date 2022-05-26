3fc8b9e4ed44fd7199ac1e516d0e1604d9ee621c
Zeynep <zinfinite97@gmail.com>
2022-05-27 00:15:07 +0300

    Create baslik2.php

---
+++ baslik2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+			<a class="navbar-brand col-md-3 col-lg-3 mr-0 px-3 my-2 text-center" href="#"> <big>Güngören  Kütüphanesi</big> </a>
+			<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"> </span>
+			</button>
+
+			<nav class="nav ">
+
+				<a  class="nav-link text-white ml-4 " href="index.php"> <big>Anasayfa</big></a>
+				<a  class="nav-link text-white" href="hakkimizda.php"><big>Hakkımızda</big> </a>
+				<a  class="nav-link text-white " href="iletisim.php"><big>İletişim</big></a>
+				<a  class="nav-link  text-white" href="misyon.php"><big>Misyon & Vizyon</big></a>	
+			</nav>
+			
+		</nav>
+
+</body>
+</html>
