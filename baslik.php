b8274dacc86ccc3cb8d2402535650782fbbfab38
Zeynep <zinfinite97@gmail.com>
2022-05-27 00:14:50 +0300

    Create baslik.php

---
+++ baslik.php
@@ -0,0 +1,15 @@
+
+<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow ">
+	<a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3 my-2 text-center" href="index.php"> <big>Güngören  Kütüphanesi</big> </a>
+	<nav class="nav ">
+		<a  class="nav-link text-white ml-4 " href="index.php"> <big>Anasayfa</big></a>
+		<a  class="nav-link text-white" href="hakkimizda.php"><big>Hakkımızda</big> </a>
+		<a  class="nav-link text-white " href="iletisim.php"><big>İletişim</big></a>
+		<a  class="nav-link text-white" href="misyon.php"><big>Misyon & Vizyon</big></a>	
+	</nav>
+	<ul class="navbar-nav ">
+		<li class="nav-item text-nowrap">
+			<a class="btn btn-dark text-info " href="cikis.php">Çıkış Yap </a>
+		</li>
+	</ul>
+</nav>
