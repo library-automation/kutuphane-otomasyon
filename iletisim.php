2dc9d838ece822c35975c1dce55027cd11e0b5b4
Zeynep <zinfinite97@gmail.com>
2022-05-27 12:05:24 +0300

    Create iletisim.php

---
+++ iletisim.php
@@ -0,0 +1,24 @@
+<?php 
+
+include("header.php");
+include("baslik3.php");
+?>
+<br>
+ <div class="container">
+ 	<div class="row jumbotron">
+
+ 	<h2 class=" p-3 mb-3  "> <strong>İletişim</strong> </h2>
+ 	<div class="col-12"><ins class=" ml-4"><strong>Telefon:</strong> (553) 153 27 26 </ins><br/> <br/> </div>
+<div class="col-12"><ins class="ml-4"><strong>Fax:</strong>  +90(322) 457 50 01 </ins><br/> <br/> </div>
+ 		
+        <div class="col-12"><ins class=" ml-4"><strong> E-mail:</strong></ins> <a href ="https://mail.google.com/mail/u/0/#inbox"> 180290037@firat.edu.tr </a> <br/> <br/></div>
+        <div class="col-12">   <ins class=" ml-4"><strong> Adres : </strong>Şahinbey/GAziantep</ins><br/><br/></div>
+      
+ </div>
+
+</div>
+
+
+ </body>
+ 
+ </html>
