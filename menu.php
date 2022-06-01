306204fba487a6b18d129afc295f683f0d26bfb0
~Hatice~ <47241553+htc2823@users.noreply.github.com>
2022-06-02 01:31:09 +0300

    menü
    sistem açıldığında menü kısmında greceğimiz bilgiler eklendi

---
+++ menu.php
@@ -0,0 +1,111 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Admin Girişi</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> 
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+		<div class="sidebar-sticky">
+			<ul class="nav flex-column">
+				<li class="nav-item">
+					<br>
+					<a class="nav-link " href="kitapkayit.php" style="font-size: 120% ">
+						<span data-feather="home"></span>
+						Kitap Kayıt <span class="sr-only">(current)</span>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="adminpanel.php" style="font-size: 120%">
+						<span data-feather="file"></span>
+						Kitap Arama
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="kitapiade.php" style="font-size: 120%">
+						<span data-feather="shopping-cart"></span>
+						Kitap İade
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="kitapver.php" style="font-size: 120%">
+						<span data-feather="users"></span>
+						Kitap Ver
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="kitapguncelle.php" style="font-size: 120%">
+						<span data-feather="bar-chart-2"></span>
+						Kitap Güncelle
+					</a>
+				</li>
+			
+			</ul>
+
+
+			
+		</div>
+	</nav>
+
+
+
+	<!-- <div class="row mt-4 ">
+		<div class="col-8 ml-3  p-4">
+			<a href="adminpanel.php" class="text-dark text-uppercase mx-5 "> <strong>Kitap Arama</strong></a>
+		</div>
+	</div>
+	<div class="row " >
+		<div class="col-8  ml-3 p-4  ">
+			<a href="kitapkayit.php" class="text-dark text-uppercase mx-5  "> <strong>Kitap Kayıt</strong></a>
+		</div>
+	</div>
+	<div class="row  ">
+		<div class="col-8 ml-3  p-4">
+			<a href="kitapver.php" class="text-dark text-uppercase mx-5 "> <strong>Kitap Ver</strong></a>
+		</div>
+	</div>
+	<div class="row ">
+		<div class="col-8 ml-3 p-4">
+			<a href="kitapiade.php" class="text-dark text-uppercase mx-5  "> <strong>Kitap İade</strong></a>
+		</div>
+	</div>
+	<div class="row ">
+		<div class="col-8 ml-3 p-4">
+			<a href="kitapguncelle.php" class="text-dark text-uppercase mx-4 "> <strong>Kitap Güncelle</strong></a>
+		</div>
+	</div>
+	<div class="row ">
+		<div class="col-8 ml-3   p-4">
+			<a href="cikis.php" class="text-dark text-uppercase mx-5 "> <strong>Çıkış Yap </strong></a>
+		</div>
+	</div> -->
+	
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
