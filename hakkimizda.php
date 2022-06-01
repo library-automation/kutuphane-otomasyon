021cc6837918fe0c32d7a50ae8617a3998ef8f40
Zeynep <zinfinite97@gmail.com>
2022-06-01 20:16:25 +0300

    Create hakkimizda.php

---
+++ hakkimizda.php
@@ -0,0 +1,44 @@
+<?php 
+include("header.php");
+include("baslik3.php");
+
+?>
+<br>
+
+ <div class="container  ">
+ 	<div class="row jumbotron">
+ 	<h2 class=" mt-0 text-center " > <strong>Hakkımızda</strong> </h2>
+ 	<p class="mt-2 pl-4  " style ="font-size: 18px" > <em>
+ 	 Kütüphanemiz FIRAT YAZILIM'a bağlı olarak bir şeflik şeklinde kurulmuştur
+
+İlk hazırlıkları tamamlanan kütüphane 2 HAZİRAN 2022'de eski Fırat Binasında açılmıştır.
+
+
+
+Başlangıçta  587 adet kitap temin edilmiş ve 183 adet kitapta ilk etapta 
+yurt dışına sipariş edilmiştir. Ayrıca 2 Ansiklopedi serisi ve bazı bibliyografik kaynak mahiyetindeki kitaplarında
+bulunduğu 43 adet kitap KÜTÜPHANEMİZE hediye edilmiştir. Daha sonra Türkiye 
+içinden ve dışından yapılan siparişler hızlandırılarak 1 TEMMUZ 2022 tarihinde cilt sayısı 1500'e yükselmiştir.
+
+
+Alınan BLISS Kütüphane Otomasyon Programı sayesinde katalogunu bilgisayar ortamına
+aktarmış. Bu sayede de internet üzerinden e-katalog hizmeti sunmaya başlamıştır
+ 	<br/> <br>
+ 	
+ 
+ 	 
+ 	</em>
+ 	
+ </p>
+<!--  <header>
+		<nav>
+			<a  class="float-right" href="index.php"  > <strong>Çıkış Yap</strong></a>
+		</nav>
+	</header> -->
+</div>
+ </div>
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  
+ </body>
+ 
+ </html>
