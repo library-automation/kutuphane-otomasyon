4accc95df6203613cbd10d5ff3c0e0ecb006948d
~Hatice~ <47241553+htc2823@users.noreply.github.com>
2022-06-02 21:59:13 +0300

    misyon/vizyon
    misyonumuz ve vizyonumuz kısımları eklendi

---
+++ misyon.php
@@ -0,0 +1,25 @@
+<?php 
+include("header.php");
+include ("baslik3.php");
+?>
+ <br>
+ <div class="container">
+ 	<div class="row jumbotron">
+ 	<h2 class=" p-3 "> <strong>Misyonumuz : </strong> </h2>
+ 	
+ 	<p class="mt-4 pl-2 "><em> <strong> Kütüphanemiz bilgi üretimini etkin, hızlı ve verimli evrensel bilgi akışını sağlayarak desteklemek, her türlü bilgi ihtiyacında araştırmacıların kütüphanemizi başlangıç noktası olarak görmelerini sağlamak, öğrenen organizasyon olmayı ve bilgiyi paylaşmayı ilke edinmek, öğrenci odaklı eğitim anlayışını destekleyecek şekilde öğretim, araştırma faaliyetleri ve topluma hizmet için gerekli doğru, güvenilir, evrensel bilgiye çağdaş olanaklarla erişilmesini sağlamak ve örnek bir kütüphane olmak için varız.
+ </strong>
+ </em> </p>
+
+ <h2 class=" p-3 mt-4" > <strong>Vizyonumuz : </strong> </h2>
+ 	<p class="mt-4 pl-2"><em><strong> Güncel, bilimsel her türlü kaynağı bünyesinde bulundurmayı ilke edinen bir bilgi merkezi olarak kullanıcılarımız olan akademik/idari personel ve öğrencilerimize istenen nitelikte, kaliteli bilgi hizmeti sunabilen,bilgi kaynaklarına erişimin daha kolay ve hızlı olmasını sağlayan, bölgesinde referans olmayı başarmış, güçlü ve zengin bir bilgi ve belge merkezi olabilmektir. </strong>
+ </em> </p>
+ 
+</div>
+ </div>
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    
+
+ </body>
+ 
+ </html>
